5369ab26727dace358315f73ac1b1877e3fbb9fc
aleehjoo <alejandroumila2@gmail.com>
2025-08-02 11:19:36 +0800

    Finished continuation of arrays GT

---
+++ gt2/src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        //2D Array with 4 rows 3 columns, any value
+        //Print in for loop and enhanced for loop
+
+        int[][] arr = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9},
+                {10, 11, 12}
+        };
+
+        System.out.println("For loop");
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\nEnhanced for loop");
+        for (int[] row : arr) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
